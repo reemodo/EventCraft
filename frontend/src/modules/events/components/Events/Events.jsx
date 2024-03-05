20e14ae1d3ba52467c47b8b55eef91e242777a9a
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-03-05 15:07:45 +0200

    ..

--- frontend/src/modules/events/components/Events/Events.jsx
+++ frontend/src/modules/events/components/Events/Events.jsx
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 export const Events = () => {
   const { data: events, isLoading } = useGetEventsQuery();
 
-  const eventRdx = useSelector((state) => state.events);
+  const eventRdx = useSelector((state) => state.events); //only for demonstration
 
   return <div>{eventRdx.selectedEvent.name}</div>;
 };
