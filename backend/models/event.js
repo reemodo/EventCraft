a85d03361aee7d94c25cda146da738d5c762d18e
SujoodEldda <sujood.eldda@gmail.com>
2024-03-05 15:39:12 +0200

    removing checking lines

--- backend/models/event.js
+++ backend/models/event.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const EventSchema = new Schema({
-  // _id: String,
+  _id: String,
   userId: String,
   title: String,
   description: String,
