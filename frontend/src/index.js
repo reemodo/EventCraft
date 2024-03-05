9c0de592b6e3ed78bb42dd80b6f543120145ff7b
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-03-05 17:16:46 +0200

    add login form modal

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -9,6 +9,11 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import { store } from "./rdx/store";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
