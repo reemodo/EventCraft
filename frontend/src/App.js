435519d5f7f3438bbf3fd130d64af47c9fd59a0a
Naarani <n3ra96@gmail.com>
2024-03-06 02:16:43 +0200

    created NavBAr folder and Navbar.js in it ,inside Navbar.js there's our logo(currently text) home link and log link (i mimiced our sketch so this will probably be changed later)  , added router component to App.js with paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,23 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from "./modules/NavBar/Navbar";
+import Home from "./modules/home/Home";
+import { AuthModal } from "./modules/auth/components/AuthModal/AuthModal";
+
 
 function App() {
+  return (
   <Router>
-    return <div className="App"></div>;
+    <div className="App">
+      <Navbar/>
+    </div>
     <Routes>
-      <Route/>
+      <Route path="/" element={<Home/>} />
+      <Route path="/log" element={<AuthModal/>} />
     </Routes>
 
   </Router>
+  );
 }
 
 export default App;
