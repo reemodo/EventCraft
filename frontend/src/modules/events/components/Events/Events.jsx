21bc356a0a6270139a7a3624a9961d2fd2b5321c
ReemSh <reem.shamasnah@gmail.com>
2024-03-06 21:16:26 +0200

    view cards

--- frontend/src/modules/events/components/Events/Events.jsx
+++ frontend/src/modules/events/components/Events/Events.jsx
@@ -1,14 +1,37 @@
-import React from "react";
-import { useGetEventsQuery } from "../../api/events.api";
-import { Event } from "./Event";
-import dummyData from "./../../../../event";
-export const Events = () => {
-  // const { data: events, isLoading } = useGetEventsQuery();
-  
-  // const eventRdx = useSelector((state) => state.events); //only for demonstration
-  // return <div>{eventRdx.selectedEvent.name}</div>;
-  return (<>
-            {dummyData.map(event => <Event event = {event}  key={event.id} /> )}
-          </>
-  )
-};
+import React from 'react'
+import dummyData from '../../../../event'
+import { EventCard } from './EventCard'
+import {Grid, Typography } from '@mui/material';
+
+export function Events(props) {
+
+    const groupedEvents = dummyData.reduce((event, obj) => {
+        const { category, ...rest } = obj;
+        if (!event[category]) {
+            event[category] = [rest]; // Create a new array for the category
+        } else {
+            event[category].push(rest); // Push the object to the existing array
+        }
+        return event;
+      }, {});
+    return (
+        <>
+        <div>
+      {Object.entries(groupedEvents).map(([category, items]) => (
+        <div key={category}>
+          <Typography variant="h4" gutterBottom>
+            {category}
+          </Typography>
+          <Grid container spacing={2}>
+            {items.map((event, index) => (
+              <Grid item key={index} xs={12} sm={6} md={3}>
+                <EventCard event={event} />
+              </Grid>
+            ))}
+          </Grid>
+        </div>
+      ))}
+    </div>
+        </>
+    )
+}
