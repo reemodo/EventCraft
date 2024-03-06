a23d28f8226dab6385a460be20066fae537e0514
SujoodEldda <sujood.eldda@gmail.com>
2024-03-06 09:54:59 +0200

    adding the user collection manager

--- backend/server/routes/events_API.js
+++ backend/server/routes/events_API.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const DBManager = require("../events-DB-Server");
+const userCollManager = require("../collections-manager/userCollManager");
 
 router.get("/DBgenerator", async function (req, res) {
   try {
     await DBManager.reGenerate();
     res.end();
+  } catch (err) {
+    console.error(err);
+    res.status(400).send((err) => err);
+  }
+});
+
+router.get("/", async function (req, res) {
+  try {
+    const user = await userCollManager.getUsers();
+    res.send(user);
   } catch (err) {
     console.error(err);
     res.status(400).send((err) => err);
