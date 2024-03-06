250ef714b4d0ffbddb2db1b10f095801f1ce2670
ReemSh <reem.shamasnah@gmail.com>
2024-03-06 09:34:07 +0200

    add event component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
-
+import {Home} from "./modules/home/Home"
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Home/>
+    
+  </div>;
 }
 
 export default App;
