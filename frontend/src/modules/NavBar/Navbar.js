bec577c00fbfa5cedc52022fe908ae8c8be9cfbb
Naarani <n3ra96@gmail.com>
2024-03-06 02:26:40 +0200

    improved the Navbar design

--- frontend/src/modules/NavBar/Navbar.js
+++ frontend/src/modules/NavBar/Navbar.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 
 const Navbar = () => {
   return (
+    
     <nav className="navbar">
-        eventCraft
-        <div className="navbar-link">
-        <Link to="/">Home</Link>
-        </div>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
 
-        <div className="navbar-link">
-          <Link to="/log">log in </Link>
-        </div>
-        
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            eventCraft
+          </Typography>
+
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Link to="/">Home</Link>
+          </Typography>
+
+          <Button color="inherit"><Link to="/log">Login</Link></Button>
+
+        </Toolbar>
+      </AppBar>
+    </Box>
+           
     </nav>
   );
 }
