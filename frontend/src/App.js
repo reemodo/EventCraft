691d7c04d8dc58dfe7902d5c4a295f503e66737c
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-03-06 11:02:10 +0200

    add event Form module

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { EventFormModal } from "./modules/events/components/EventFormModal/EventFormModal";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <EventFormModal isOpen={true} />
+      </LocalizationProvider>
+    </div>
+  );
 }
 
 export default App;
