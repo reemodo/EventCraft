bb8f87edba2db263fecfc0147c1686c50984e479
Naarani <n3ra96@gmail.com>
2024-03-07 11:44:26 +0200

    added EventPage.js  title,info,time,location with a box showing the map of the location

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Navbar from "./modules/NavBar/Navbar";
 import Home from "./modules/home/Home";
 
 import { AuthModal } from "./modules/auth/components/AuthModal/AuthModal";
+import EventPage from "./modules/events/pages/EventPage";
 
 function App() {
   
@@ -30,7 +31,16 @@ function App() {
             <Navbar/>
           </div>
           <Routes>
-            <Route path="/" element={<Home/>} />
+            {/* <Route path="/" element={<Home/>} /> */}
+            <Route path="/" element={<EventPage  
+                imageUrl="\Frame-1-3-min-1-3.png"
+                title="Fattoush"
+                time="January 1, 2025, 10:00 AM"
+                location="Ha-Namal St 6, Haifa"
+                info="Great vibes, amazing interior with deep attention to details, nice  menu and snack to accompany it. There is  DJ  playing cool and chill music!."
+                lat="32.81781057069659"
+                lng="35.00259862330999"/>} />
+                
             <Route path="/log" element={<AuthModal/>} />
           </Routes>
 
