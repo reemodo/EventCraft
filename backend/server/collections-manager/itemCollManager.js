c85dad931cdca653530129611819e1941fc25ce4
SujoodEldda <sujood.eldda@gmail.com>
2024-03-11 15:27:11 +0200

    adding the item routes

--- backend/server/collections-manager/itemCollManager.js
+++ backend/server/collections-manager/itemCollManager.js
@@ -1,10 +1,12 @@
-const ItemSchema = require("../../models/Item");
+const mongoose = require("mongoose");
+const ItemSchema = mongoose.model("ItemSchema", require("../../models/Item"));
 const Card = require("../../models/card");
 const { findUpdatedItemFields } = require("../../utility");
+const { updateCardFields } = require("./cardCollManager");
 
 class itemCollManager {
   static async getItems(cardId) {
-    const items = await Card.find({ cardId }).select("cardItems");
+    const items = await Card.find({ _id: cardId }).select("cardItems");
     return items;
   }
   static async deleteItem(cardId, itemId) {
@@ -14,7 +16,7 @@ class itemCollManager {
         return { success: false, error: "Card not found" };
       }
       const updatedCardItems = card.cardItems.filter(
-        (item) => item._id !== itemId
+        (item) => item._id != itemId
       );
       card.cardItems = updatedCardItems;
       await card.save();
@@ -30,11 +32,8 @@ class itemCollManager {
       if (!card) {
         return { success: false, error: "Card not found" };
       }
-      const lastItemId = await itemCollManager.findTheLastItem();
-      const newItem = new ItemSchema({
-        _id: lastEventId + 1,
-        ...newItemData,
-      });
+      const lastItemId = await itemCollManager.findTheLastItem(cardId);
+      const newItem = new ItemSchema({ _id: lastItemId, ...newItemData });
       card.cardItems.push(newItem);
       await card.save();
       return { success: true, message: "Item saved successfully" };
@@ -44,20 +43,20 @@ class itemCollManager {
   }
 
   static async findTheLastItem(cardId) {
-    const card = await Card.find({ cardId }).sort({ _id: -1 }).limit(1);
-    return card ? card[0]._id : -1;
+    const card = await Card.find({ _id: cardId }).sort({ _id: -1 }).limit(1);
+    if (card[0].cardItems.length == 0) return 0;
+    else return card[0].cardItems.length;
   }
 
   static async editItemPosition(cardId, itemId, newPosition) {
     try {
       const updateFields = findUpdatedItemFields(newPosition);
-      const updatedCard = await Card.updateOne(
+      const updatedCard = await Card.findOneAndUpdate(
         { _id: cardId, "cardItems._id": itemId },
-        {
-          $set: updateFields,
-        }
+        { $set: updateFields },
+        { new: true }
       );
-      if (!updatedCard.nModified) {
+      if (!updatedCard) {
         return { success: false, error: "Card or Item not found" };
       }
       return { success: true, message: "Item position updated successfully" };
