0964ff0bb5f6ff031b7c887f6164096d4c2ffe5d
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-03-11 17:31:52 +0200

    add images and shapes to edit card

--- backend/server/routes/unSplash_API.js
+++ backend/server/routes/unSplash_API.js
@@ -25,9 +25,38 @@ router.get("/api/photos", async (req, res) => {
       page: 1,
       search: query.search || "",
     });
-    const photos = response.response.results;
+    const photos = response.response.results.map((photo) => ({
+      url: photo.urls.small,
+      id: photo.id,
+    }));
 
     res.json(photos);
+  } catch (error) {
+    console.error("Error fetching photos:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/api/shapes", async (req, res) => {
+  const { search } = req.query;
+
+  // const baseUrl =
+  //   "https://api.freepik.com/v1/resources?locale=en-US&page=1&limit=10&order=latest&term=shapes&type=icon";
+
+  const q = process.env.REACT_APP_FREE_PIK_API_KEY;
+
+  const baseUrl2 = "https://api.freepik.com/v1/icons";
+
+  try {
+    const shapes = await axios.get(baseUrl2, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "X-Freepik-API-Key": process.env.REACT_APP_FREE_PIK_API_KEY,
+      },
+    });
+
+    res.json(shapes.data);
   } catch (error) {
     console.error("Error fetching photos:", error);
     res.status(500).json({ error: "Internal Server Error" });
