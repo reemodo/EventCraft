65064a2acf7cb872f62073619133d968deccd866
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-03-11 15:44:56 +0200

    ..

---
+++ frontend/src/modules/shared/hooks/useUnsplash/useUnsplashHelpers.jsx
@@ -0,0 +1,26 @@
+import React, { useCallback } from "react";
+
+import { useLazyGetPhotosQuery } from "../../../shared/apis/unsplash.api";
+
+export const useUnsplashHelpers = () => {
+  const [getPhotosQuery, { isLoading: pendingPhotos }] =
+    useLazyGetPhotosQuery();
+
+  const getPhotos = useCallback(
+    async ({ search }) => {
+      try {
+        const photos = await getPhotosQuery({ search });
+        return photos.data;
+      } catch (err) {
+        console.log(err);
+        return [];
+      }
+    },
+    [getPhotosQuery]
+  );
+
+  return {
+    getPhotos,
+    pendingPhotos,
+  };
+};
--- frontend/src/rdx/store.js
+++ frontend/src/rdx/store.js
@@ -3,7 +3,8 @@ import { configureStore } from "@reduxjs/toolkit";
 import { eventApi } from "../modules/events/api/events.api";
 import { eventReducer } from "../modules/events/rdx/events.rdx";
 import { userReducer } from "../modules/users/rdx/users.rdx";
-import { userApi } from "../modules/events/api/user.api";
+import { userApi } from "../modules/users/api/user.api";
+import { unsplashApi } from "../modules/shared/apis/unsplash.api";
 
 const setupStore = () => {
   return configureStore({
@@ -15,10 +16,15 @@ const setupStore = () => {
 
       [eventApi.reducerPath]: eventApi.reducer,
       [userApi.reducerPath]: userApi.reducer,
+      [unsplashApi.reducerPath]: unsplashApi.reducer,
     },
 
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat([eventApi.middleware, userApi.middleware]),
+      getDefaultMiddleware().concat([
+        eventApi.middleware,
+        userApi.middleware,
+        unsplashApi.middleware,
+      ]),
   });
 };
 
