a02f98044e2455bbb7865936326639dc1954d32c
ReemSh <reem.shamasnah@gmail.com>
2024-03-12 14:18:00 +0200

    add action list

--- frontend/src/modules/events/components/Events/EventCard.jsx
+++ frontend/src/modules/events/components/Events/EventCard.jsx
@@ -12,18 +12,25 @@ import { useTheme } from "@mui/material";
 import { ActionsList } from "./ActionsList"
 import { rdxEventsActions } from "../../rdx/events.rdx";
 import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-export const EventCard = ({ event, inHomePage, editModel }) => {
+export const EventCard = ({ event, inHomePage }) => {
   const theme = useTheme();
+  const navigate = useNavigate()
   const dispatch = useDispatch();
   
   const handelSelectedEvent=()=>{
     dispatch(rdxEventsActions.setSelectedEvent(event));
   }
-
+  const handelEventClick = (event) => {
+    if (inHomePage) {
+      dispatch(rdxEventsActions.setSelectedEvent(event));
+      navigate(`/eventPage`);
+    } 
+  };
   return (
     <>
-      <Card sx={{ width: 350 }} >
+      <Card sx={{ width: 350 }} onClick={handelEventClick}>
         <CardActionArea>
           <CardMedia
             component="img"
