3007f8eb762b3a39314053395273f792437b66fe
Naarani <n3ra96@gmail.com>
2024-03-13 14:23:39 +0200

    location lng & lat autocomplete COMPLETED!

--- backend/models/event.js
+++ backend/models/event.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const EventSchema = new Schema({
-  _id: Number,
   userId: Number,
   title: String,
   description: String,
