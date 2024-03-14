745f103af50f5bb185802b605e367b01596bf640
asadhmeedrunn <asad.hmeed@runnableweb.com>
2024-03-14 20:54:19 +0200

    show the side bar only when uesr is logged in

--- frontend/src/modules/NavBar/Navbar.js
+++ frontend/src/modules/NavBar/Navbar.js
@@ -17,10 +17,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import List from "@mui/material/List";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import Divider from "@mui/material/Divider";
-import {
-  EventCardListItems,
-  MainListItemsMenu
-} from "../landing/Dashbord";
+import { EventCardListItems, MainListItemsMenu } from "../landing/Dashbord";
 import { useSelector } from "react-redux";
 import { useInit } from "../shared/hooks/useInit/useInit";
 
@@ -105,24 +102,26 @@ const Navbar = () => {
       <Box sx={{ flexGrow: 1 }}>
         <CssBaseline />
 
-        <AppBar position="absolute" open={open}>
+        <AppBar position="absolute" open={rdxUser.loggedIn ? open : false}>
           <Toolbar
             sx={{
               pr: "24px", // keep right padding when drawer closed
             }}
           >
-            <IconButton
-              edge="start"
-              color="secondary"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: "36px",
-                ...(open && { display: "none" }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
+            {rdxUser.loggedIn && (
+              <IconButton
+                edge="start"
+                color="secondary"
+                aria-label="open drawer"
+                onClick={toggleDrawer}
+                sx={{
+                  marginRight: "36px",
+                  ...(open && { display: "none" }),
+                }}
+              >
+                <MenuIcon />
+              </IconButton>
+            )}
             <Typography
               component="h1"
               variant="h6"
@@ -146,7 +145,7 @@ const Navbar = () => {
               </Badge>
             </IconButton>
 
-            <Button
+            {/* <Button
               color="inherit"
               style={{
                 color: theme.palette.secondary.main,
@@ -154,7 +153,7 @@ const Navbar = () => {
               onClick={onLogin}
             >
               About us
-            </Button>
+            </Button> */}
             {!rdxUser.loggedIn && (
               <Button
                 color="inherit"
@@ -180,33 +179,34 @@ const Navbar = () => {
             )}
           </Toolbar>
         </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            <MainListItemsMenu />
 
-            {rdxEvents.isEditingEventCard && (
-              <>
-                <Divider sx={{ my: 1 }} />
-                <EventCardListItems />
-              </>
-            )}
+        {rdxUser.loggedIn && (
+          <Drawer variant="permanent" open={open}>
+            <Toolbar
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+                px: [1],
+              }}
+            >
+              <IconButton onClick={toggleDrawer}>
+                <ChevronLeftIcon />
+              </IconButton>
+            </Toolbar>
+            <Divider />
+            <List component="nav">
+              <MainListItemsMenu />
 
-        
-          </List>
-        </Drawer>
+              {rdxEvents.isEditingEventCard && (
+                <>
+                  <Divider sx={{ my: 1 }} />
+                  <EventCardListItems />
+                </>
+              )}
+            </List>
+          </Drawer>
+        )}
 
         <AuthModal isOpen={openAuthModal} onClose={onCloseLogin} />
       </Box>
