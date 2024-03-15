25850da02a8cbfe34e29a21952b181718f6d5654
asadhmeed2 <asadhmeed2@gmail.com>
2024-03-15 12:47:29 +0200

    ..

--- frontend/src/modules/events/hooks/useEventHelper.js
+++ frontend/src/modules/events/hooks/useEventHelper.js
@@ -1,14 +1,22 @@
 import { useCallback, useState } from "react";
-import { useJoinEventMutation, useLazyGetEventQuery } from "../api/events.api";
+import {
+  useCancelJoinedEventMutation,
+  useJoinEventMutation,
+  useLazyGetEventQuery,
+} from "../api/events.api";
 
 export const useEventHelpers = () => {
   const [pendingGetEvent, setPendingEvent] = useState(false);
   const [isErrorGetEvent, setIsErrorEvent] = useState(false);
   const [pendingJoinEvent, setPendingJoinEvent] = useState(false);
+  const [pendingCancelJoinedEvent, setPendingCancelJoinedEvent] =
+    useState(false);
 
   const [getEventApi] = useLazyGetEventQuery();
 
   const [joinEventApi] = useJoinEventMutation();
+
+  const [cancelJoinEventApi] = useCancelJoinedEventMutation();
 
   const getEvent = useCallback(
     async (id) => {
@@ -43,11 +51,28 @@ export const useEventHelpers = () => {
     [joinEventApi]
   );
 
+  const cancelJoinedEvent = useCallback(
+    async (userId, eventId) => {
+      try {
+        setPendingCancelJoinedEvent(true);
+        const eventJoined = await cancelJoinEventApi({ userId, eventId });
+        return eventJoined.data;
+      } catch (err) {
+        console.log("🚀 ~ joinEvent ~ err:", err);
+      } finally {
+        setPendingCancelJoinedEvent(false);
+      }
+    },
+    [joinEventApi]
+  );
+
   return {
     getEvent,
     joinEvent,
+    cancelJoinedEvent,
     pendingGetEvent,
     isErrorGetEvent,
     pendingJoinEvent,
+    pendingCancelJoinedEvent,
   };
 };
