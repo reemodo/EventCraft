94650a77ae00de57fde3f31e79f3c13ee2d6d833
asadhmeed2 <asadhmeed2@gmail.com>
2024-03-15 15:04:39 +0200

    ..

--- frontend/src/modules/events/pages/home/Home.jsx
+++ frontend/src/modules/events/pages/home/Home.jsx
@@ -5,7 +5,8 @@ import { useEffect } from "react";
 import Layout from "../../../landing/Layout";
 import { TopContainer } from "./TopContainer";
 import { useGetEvents } from "../../hooks/useGetEvents";
-import FilterForm from './FilterForm';
+import FilterForm from "./FilterForm";
+
 export function Home(props) {
   const [eventsList, setEventsList] = useState([]);
   const [filteredEvents, setFilteredEvents] = useState({});
@@ -14,19 +15,15 @@ export function Home(props) {
 
   useEffect(() => {
     (async () => {
-      try{
-
+      try {
         const data = await fetchEvents(filteredEvents);
-  
+
         if (data) {
           setEventsList(data);
         }
-      }
-      catch{
-
-      }
+      } catch {}
     })();
-  }, [fetchEvents,filteredEvents]);
+  }, [fetchEvents, filteredEvents]);
 
   const onJoinEvent = (event, userId) => {
     const eventsListClone = [...eventsList];
@@ -34,33 +31,35 @@ export function Home(props) {
 
     const updatedEvent = { ...eventsListClone[idx] };
 
-    updatedEvent.attendance = [...updatedEvent.attendance, userId];
+    updatedEvent.attendance = [...updatedEvent.attendance, { _id: userId }];
 
     eventsListClone.splice(idx, 1, updatedEvent);
 
     setEventsList(eventsListClone);
   };
 
-  const onCancelJoinEvent=  (event, userId) => {
+  const onCancelJoinEvent = (event, userId) => {
     const eventsListClone = [...eventsList];
     const idx = eventsListClone.findIndex((evt) => event._id === evt._id);
 
     const updatedEvent = { ...eventsListClone[idx] };
 
-    updatedEvent.attendance =  [...updatedEvent.attendance.filter((id) => id !== userId)];
+    updatedEvent.attendance = [
+      ...updatedEvent.attendance.filter((user) => user._id !== userId),
+    ];
 
     eventsListClone.splice(idx, 1, updatedEvent);
 
     setEventsList(eventsListClone);
   };
-  
+
   const handleFilter = (filters) => {
-    const newFilter = {...filters, title : filteredEvents.title};
+    const newFilter = { ...filters, title: filteredEvents.title };
     setFilteredEvents(newFilter);
   };
-  
+
   const handelSearch = (title) => {
-    const newFilter = {...filteredEvents, title}
+    const newFilter = { ...filteredEvents, title };
     setFilteredEvents(newFilter);
   };
   if (isLoading) {
@@ -74,10 +73,15 @@ export function Home(props) {
   return (
     <>
       <Layout>
-        <TopContainer events={eventsList}  handelSearch={handelSearch}/>
-        <FilterForm eventsList={eventsList} onFilter={ handleFilter}/>
+        <TopContainer events={eventsList} handelSearch={handelSearch} />
+        <FilterForm eventsList={eventsList} onFilter={handleFilter} />
         <div className="homeContainer">
-          <Events inHomePage={true} events={eventsList} onJoinEvent={onJoinEvent}  onCancelJoinEvent={onCancelJoinEvent}/>
+          <Events
+            inHomePage={true}
+            events={eventsList}
+            onJoinEvent={onJoinEvent}
+            onCancelJoinEvent={onCancelJoinEvent}
+          />
         </div>
       </Layout>
     </>
