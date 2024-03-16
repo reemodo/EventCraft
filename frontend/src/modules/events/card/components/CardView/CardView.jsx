47cf46b2daa472284f6c35fef408c180b9959c88
asadhmeed2 <asadhmeed2@gmail.com>
2024-03-16 09:56:40 +0200

    fix create event image bug and edit event bug

--- frontend/src/modules/events/card/components/CardView/CardView.jsx
+++ frontend/src/modules/events/card/components/CardView/CardView.jsx
@@ -5,39 +5,36 @@ import { CardListItem } from "../CardListItem/CardListItem";
 import { Box } from "@mui/material";
 
 export const CardView = forwardRef(({ title, item, model }, ref) => {
+  const list = !!model
+    ? model.cardItems
+    : [
+        {
+          ...item,
+          text: title,
+        },
+      ];
+
   return (
-    <Box ref={ref}>
-      <Box
-        sx={{
-          position: "relative",
-          width: "500px",
-          height: "300px",
-          boxShadow: "rgba(17, 12, 46, 0.08) 0px 48px 100px 0px",
-          mt: 2,
-        }}
-        overflow={"hidden"}
-      >
-        {!model && (
+    <Box
+      sx={{
+        position: "relative",
+        width: "500px",
+        height: "300px",
+        backgroundColor: "white",
+        mt: 2,
+      }}
+      overflow={"hidden"}
+      ref={ref}
+    >
+      <Box sx={{ width: "100%", height: "100%" }}>
+        {list.map((item) => (
           <CardListItem
             item={{
               ...item,
-              text: title,
             }}
+            key={item._id}
           />
-        )}
-
-        {!!model && (
-          <>
-            {model.cardItems.map((item) => (
-              <CardListItem
-                item={{
-                  ...item,
-                }}
-                key={item._id}
-              />
-            ))}
-          </>
-        )}
+        ))}
       </Box>
     </Box>
   );
