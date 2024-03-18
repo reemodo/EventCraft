c1192bab225daebc8b5e22364012541ee2c45c73
asadhmeed2 <asadhmeed2@gmail.com>
2024-03-18 12:10:13 +0200

    add image to the event card on create via category

--- frontend/src/modules/events/card/components/CardView/CardView.jsx
+++ frontend/src/modules/events/card/components/CardView/CardView.jsx
@@ -3,48 +3,44 @@ import React, { forwardRef } from "react";
 import { CardListItem } from "../CardListItem/CardListItem";
 
 import { Box } from "@mui/material";
-import { ItemTypes } from "../CardEdit/CardEdit";
 
-export const CardView = forwardRef(({ title, item, model, bgUrl }, ref) => {
-  const list = !!model
-    ? model.cardItems
-    : [
-        {
-          ...item,
-          text: title,
-        },
-        {
-          type: bgUrl ? ItemTypes.IMAGE : "",
-          width: "100%",
-          height: "100%",
-          src: bgUrl,
-          top: 0,
-          left: 0,
-        },
-      ];
+export const CardView = forwardRef(
+  ({ title, item, model, bgItem, bgUrl }, ref) => {
+    const list = !!model
+      ? model.cardItems
+      : [
+          {
+            ...item,
+            text: title,
+          },
+        ];
 
-  return (
-    <Box
-      sx={{
-        position: "relative",
-        width: "500px",
-        height: "300px",
-        backgroundColor: "white",
-        mt: 2,
-      }}
-      overflow={"hidden"}
-      ref={ref}
-    >
-      <Box sx={{ width: "100%", height: "100%" }}>
-        {list.map((item) => (
-          <CardListItem
-            item={{
-              ...item,
-            }}
-            key={item._id}
-          />
-        ))}
+    const listWithBgImag =
+      !model && bgUrl ? [...list, { ...bgItem, src: bgUrl }] : list;
+
+    return (
+      <Box
+        sx={{
+          position: "relative",
+          width: "500px",
+          height: "300px",
+          backgroundColor: "white",
+          mt: 2,
+        }}
+        overflow={"hidden"}
+        ref={ref}
+      >
+        <Box sx={{ width: "100%", height: "100%" }}>
+          {listWithBgImag.map((item, idx) => (
+            <CardListItem
+              item={{
+                ...item,
+              }}
+              key={item._id || idx}
+            />
+          ))}
+        </Box>
       </Box>
-    </Box>
-  );
-});
+    );
+  }
+);
