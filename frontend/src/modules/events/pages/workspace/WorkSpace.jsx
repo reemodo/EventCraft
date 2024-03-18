fc876edd4e548ffea1b545ae43191c2b2061f414
Fadi <fadi.97.kh@outlook.com>
2024-03-18 19:55:25 +0200

    snackbar for add/delete event

--- frontend/src/modules/events/pages/workspace/WorkSpace.jsx
+++ frontend/src/modules/events/pages/workspace/WorkSpace.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Events } from "../../components/Events/Events";
 import "./workSpace.css";
 
@@ -6,15 +6,11 @@ import {
   Box,
   Button,
   CircularProgress,
+  Snackbar,
   Stack,
   Typography,
 } from "@mui/material";
-import { useState } from "react";
-
-import { useEffect } from "react";
-
-import Layout from "../../../landing/Layout";
-
+import { Alert } from "@mui/material";
 import { useGetMyEvents } from "../../hooks/useGetMyEvents";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../home/SearchBar";
@@ -22,20 +18,23 @@ import SearchBar from "../home/SearchBar";
 export const WorkSpace = (props) => {
   const navigate = useNavigate();
   const [filteredEvents, setFilteredEvents] = useState({});
-  const onOpenCreateModel = () => {
-    navigate("/addEvent");
-  };
-
   const [eventsList, setEventsList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [snackbarOpen, setSnackbarOpen] = useState(true);
+  const [snackbarMessage, setSnackbarMessage] = useState("Event Added Succefully");
 
-  const { isLoading, error, fetchMyEvents } = useGetMyEvents();
+  const { fetchMyEvents } = useGetMyEvents();
 
   useEffect(() => {
     (async () => {
-      const data = await fetchMyEvents();
-
-      if (data) {
+      try {
+        const data = await fetchMyEvents();
         setEventsList(data);
+        setIsLoading(false);
+      } catch (error) {
+        setError(error);
+        setIsLoading(false);
       }
     })();
   }, [fetchMyEvents]);
@@ -43,63 +42,83 @@ export const WorkSpace = (props) => {
   const onDeleteEvent = function (eventId) {
     const newEvents = eventsList.filter((event) => event._id !== eventId);
     setEventsList(newEvents);
+    setSnackbarMessage("Event deleted successfully.");
+    setSnackbarOpen(true);
   };
+
   const handelSearch = (title) => {
     const newFilter = { ...filteredEvents, title };
     setFilteredEvents(newFilter);
   };
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  }
+  const onOpenCreateModel = () => {
+    navigate("/addEvent");
+  };
 
   return (
     <>
-      <Layout>
-        <Stack m={5}>
-          <Typography fontSize={'3vw'} fontFamily={'Lora'} fontWeight={900} component="h1" mb={3} m={5} variant="h6" alignSelf={'center'}>
-            Which event you will manage today
-          </Typography>
-          <div className="iconContainer">
-            <SearchBar handelSearch={handelSearch} />
-            <Button
-              onClick={onOpenCreateModel}
-              sx={{
-                color: "secondary.contrastText",
-                backgroundColor: "secondary.main",
-                "--Grid-borderWidth": "1px",
-                borderRadius: "40px",
-                borderTop: "var(--Grid-borderWidth) solid",
-                borderLeft: "var(--Grid-borderWidth) solid",
-                borderRight: "var(--Grid-borderWidth) solid",
-                borderBottom: "var(--Grid-borderWidth) solid",
-                borderColor: "secondary.main",
-                minWidth: "fit-content",
-                minHeight: "fit-content",
-              }}
-              variant="contained"
-              className="addButton"
-            >
-              Add Event
-            </Button>
-          </div>
-          {!isLoading && (
-            <Events
-              inHomePage={false}
-              events={eventsList}
-              handelSetEventLists={onDeleteEvent}
-            />
-          )}
-          {isLoading && (
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <CircularProgress color="secondary" />
-            </Box>
-          )}
-        </Stack>
-      </Layout>
+      <Stack m={5}>
+        <Typography
+          fontSize={"3vw"}
+          fontFamily={"Lora"}
+          fontWeight={900}
+          component="h1"
+          mb={3}
+          m={5}
+          variant="h6"
+          alignSelf={"center"}
+        >
+          Which event you will manage today
+        </Typography>
+        <div className="iconContainer">
+          <SearchBar handelSearch={handelSearch} />
+          <Button
+            onClick={onOpenCreateModel}
+            sx={{
+              color: "secondary.contrastText",
+              backgroundColor: "secondary.main",
+              "--Grid-borderWidth": "1px",
+              borderRadius: "40px",
+              borderTop: "var(--Grid-borderWidth) solid",
+              borderLeft: "var(--Grid-borderWidth) solid",
+              borderRight: "var(--Grid-borderWidth) solid",
+              borderBottom: "var(--Grid-borderWidth) solid",
+              borderColor: "secondary.main",
+              minWidth: "fit-content",
+              minHeight: "fit-content",
+            }}
+            variant="contained"
+            className="addButton"
+          >
+            Add Event
+          </Button>
+        </div>
+        {!isLoading && (
+          <Events
+            inHomePage={false}
+            events={eventsList}
+            handelSetEventLists={onDeleteEvent}
+          />
+        )}
+        {isLoading && (
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <CircularProgress color="secondary" />
+          </Box>
+        )}
+      </Stack>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={() => setSnackbarOpen(false)}
+      >
+        <Alert
+          onClose={() => setSnackbarOpen(false)}
+          severity="success"
+          sx={{ width: "100%", color: "green" }}
+        >
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
     </>
   );
 };
